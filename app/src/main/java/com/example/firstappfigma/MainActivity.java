e3afb96dca588dc3529aa18ca6f76ca9485a0e4b
Angga Risky Setiawan <anggariskysetiawan@Anggas-MacBook-Pro.local>
2019-03-10 23:10:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firstappfigma/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.firstappfigma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Animation smalltobig, btta, btta2;
+    private TextView textView, subtitle_header;
+    private Button button;
+    private EditText editText, editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // load this animation
+        smalltobig = AnimationUtils.loadAnimation(this, R.anim.smalltobig);
+        btta = AnimationUtils.loadAnimation(this, R.anim.btta);
+        btta2 = AnimationUtils.loadAnimation(this, R.anim.btta2);
+
+        imageView = findViewById(R.id.imageView);
+
+        textView = findViewById(R.id.textView);
+        subtitle_header = findViewById(R.id.subtitle_header);
+
+        button = findViewById(R.id.button);
+
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+
+        // passing animation and start it
+        imageView.startAnimation(smalltobig);
+
+        textView.startAnimation(btta);
+        subtitle_header.startAnimation(btta);
+
+        button.startAnimation(btta2);
+
+        editText.startAnimation(btta2);
+        editText2.startAnimation(btta2);
+
+
+    }
+}
